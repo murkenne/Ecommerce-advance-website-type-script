7e7ed18afbc403eca909ccc9e9834fcdb31140ea
Kenneth Murray <kwmwins@gmail.com>
2025-02-04 13:06:43 -0700

    firebase authenticate and firestore installed

---
+++ Ecommerce-store/src/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHgbgkTo0qyM1uukP2l59qBnL5Gj0tUiE",
+  authDomain: "ecommerce-store-74aaa.firebaseapp.com",
+  projectId: "ecommerce-store-74aaa",
+  storageBucket: "ecommerce-store-74aaa.firebasestorage.app",
+  messagingSenderId: "679902149617",
+  appId: "1:679902149617:web:bddcda6d7e5ae706e7a250"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
